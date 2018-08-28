a9165dd5fdf6e08629d447b96e4d8ac56a142b11
JonathanNotebook\Jonathan <joradab@gmail.com>
2018-08-28 10:40:38 +1000

    changed test file

--- create_test_file.py
+++ create_test_file.py
@@ -4,5 +4,5 @@
     # 1. Extract 1000 from the real data set
     # 2. Create 1000 randomly
     # 3. remove the real edges from the random set
-    def create_test_file():
-        pass
+def create_test_file():
+    pass
