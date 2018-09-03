025c2fb749327dc0eefe61a38b00402a0c3b1e98
myrionsc <marpht23@gmail.com>
2018-09-03 09:52:16 +0845

    made data analysis more general and added edges analysed

--- data-analysis.py
+++ data-analysis.py
@@ -30,68 +30,64 @@ Comment: We should try to make the Real and Fake edges result look somewhat simi
 
 '''
 
-
-
-
-
-
-
 def main ():
 
     ### Load training data
-    print("Training and test data loading...")
+    print("Prepairing data...")
     data_df = pd.read_csv("data/generated-data.txt", sep="\t", index_col="Id")
     test_df = pd.read_csv("data/test-public.txt", sep="\t", index_col="Id")
-    print("Training and test data loaded")
 
     ### load feature extraction helper data
-    print("Feature data loading...")
     following_dict = helper.read_file("./data/train.txt")
     followers_dict = helper.read_file("./data/followers.txt")
+    print("data prepared")
+    print()
+    print()
 
     ### todo: to Jonathan: you need to outcomment 2 lines below and run to generate followers.txt file
     # followers_dict = helper.extract_followers_from_following(following_dict)
     # helper.write_dict_with_lists_to_file(followers_dict, "followers.txt")
-    print("Feature data loaded")
 
+    print("format: Following_num_avg Followers_num_avg")
+    print()
+
+    print("Real edges")
+    generated_real_edges_df = data_df[data_df.Label == True]
+    extract_followers_following_avg(generated_real_edges_df, following_dict, followers_dict)
+    print()
+
+    print("Fake edges")
+    generated_fake_edges_df = data_df[data_df.Label == False]
+    extract_followers_following_avg(generated_fake_edges_df, following_dict, followers_dict)
+    print()
+
+    print("Real and fake edges")
     generated_edges_df = data_df
+    extract_followers_following_avg(generated_edges_df, following_dict, followers_dict)
+    print()
 
+    print("Test-Public")
+    extract_followers_following_avg(test_df, following_dict, followers_dict)
+
+
+def extract_followers_following_avg(edges_df, following_dict, followers_dict):
     data_source_following_sum = 0
     data_source_followers_sum = 0
     data_sink_following_sum = 0
     data_sink_followers_sum = 0
-    for index, row in generated_edges_df.iterrows():
+    for index, row in edges_df.iterrows():
         data_source_following_sum += len(following_dict[row["Source"]])
         data_source_followers_sum += len(followers_dict[row["Source"]])
         if row["Sink"] in following_dict:
             data_sink_following_sum += len(following_dict[row["Sink"]])
         data_sink_followers_sum += len(followers_dict[row["Sink"]])
 
-    data_source_following_avg = data_source_following_sum / len(generated_edges_df)
-    data_source_followers_avg = data_source_followers_sum / len(generated_edges_df)
-    data_sink_following_avg = data_sink_following_sum / len(generated_edges_df)
-    data_sink_followers_avg = data_sink_followers_sum / len(generated_edges_df)
+    data_source_following_avg = data_source_following_sum / len(edges_df)
+    data_source_followers_avg = data_source_followers_sum / len(edges_df)
+    data_sink_following_avg = data_sink_following_sum / len(edges_df)
+    data_sink_followers_avg = data_sink_followers_sum / len(edges_df)
     print("Source: " + str(data_source_following_avg) + ": " + str(data_source_followers_avg))
     print("Sink: " + str(data_sink_following_avg) + ": " + str(data_sink_followers_avg))
-
-    print()
-    print("Test-Public")
-    test_source_following_sum = 0
-    test_source_followers_sum = 0
-    test_sink_following_sum = 0
-    test_sink_followers_sum = 0
-    for index, row in test_df.iterrows():
-        test_source_following_sum += len(following_dict[row["Source"]])
-        test_source_followers_sum += len(followers_dict[row["Source"]])
-        if row["Sink"] in following_dict:
-            test_sink_following_sum += len(following_dict[row["Sink"]])
-        test_sink_followers_sum += len(followers_dict[row["Sink"]])
-    test_src_follows_avg = test_source_following_sum / len(test_df)
-    test_src_followers_avg = test_source_followers_sum / len(test_df)
-    test_sink_follows_avg = test_sink_following_sum / len(test_df)
-    test_sink_followers_avg = test_sink_followers_sum / len(test_df)
-    print("Source: " + str(test_src_follows_avg) + ": " + str(test_src_followers_avg))
-    print("Sink: " + str(test_sink_follows_avg) + ": " + str(test_sink_followers_avg))
 
 
 if __name__ == '__main__':
