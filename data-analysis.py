655cc34d1561636382375ef0c616b3158f57bdd7
myrionsc <marpht23@gmail.com>
2018-09-05 15:49:29 +0845

    some histogram data analysis

--- data-analysis.py
+++ data-analysis.py
@@ -40,20 +40,20 @@ def main ():
 
 
     ### Load training data
-    # print("Prepairing data...")
+    print("Prepairing data...")
     data_df = pd.read_csv("data/generated-data.txt", sep="\t", index_col="Id")
     test_df = pd.read_csv("data/test-public.txt", sep="\t", index_col="Id")
 
     ### load feature extraction helper data
     following_dict = helper.read_file_to_dict("./data/train.txt")
     followers_dict = helper.read_file_to_dict("./data/followers.txt")
-    # print("data prepared")
-    # print()
-    # print()
+    print("data prepared")
+    print()
+    print()
 
 
     # num of source / sinks in train.txt
-    print("num of Source and sinks that we have data for")
+    print("num of Source and sinks that we have following data for")
     print("gen-data source: " + str(sum(data_df.Source.apply(lambda r: 1 if r in following_dict else 0))))
     print("gen-data sink: " + str(sum(data_df.Sink.apply(lambda r: 1 if r in following_dict else 0))))
     print("test-public source: " + str(sum(test_df.Source.apply(lambda r: 1 if r in following_dict else 0))))
@@ -61,11 +61,34 @@ def main ():
     print()
 
     # plot normal distribution of following numbers
-    print("Normal distribution of number of followers: (format: TITLE: MEAN, STANDARD_DEVIATION)")
-    plot_normal_distribution("gen-data source following", data_df.Source, following_dict)
-    plot_normal_distribution("gen-data sink following", data_df.Sink, following_dict)
-    plot_normal_distribution("public-test source following", test_df.Source, following_dict)
-    plot_normal_distribution("public-test sink following", test_df.Sink, following_dict)
+    # print("Normal distribution of number of followers: (format: TITLE: MEAN, STANDARD_DEVIATION)")
+    # plot_normal_distribution("gen-data source following", data_df.Source, following_dict)
+    # plot_normal_distribution("gen-data sink following", data_df.Sink, following_dict)
+    # plot_normal_distribution("public-test source following", test_df.Source, following_dict)
+    # plot_normal_distribution("public-test sink following", test_df.Sink, following_dict)
+    # print()
+    # print()
+
+    print("*** Hist plots")
+    plot_histogram("gen-data sink true following num histogram", data_df.Sink[data_df.Label == True], following_dict)
+    plot_histogram("gen-data sink false following num histogram", data_df.Sink[data_df.Label == False], following_dict)
+    plot_histogram("gen-data sink both following num histogram", data_df.Sink, following_dict)
+    plot_histogram("test-public sink following num histogram", test_df.Source, following_dict)
+    print()
+    plot_histogram("gen-data sink true followers num histogram", data_df.Sink[data_df.Label == True], followers_dict)
+    plot_histogram("gen-data sink false followers num histogram", data_df.Sink[data_df.Label == False], followers_dict)
+    plot_histogram("gen-data sink both followers num histogram", data_df.Sink, followers_dict)
+    plot_histogram("test-public sink followers num histogram", test_df.Source, followers_dict)
+    print()
+    # plot_histogram("gen-data source following num histogram", data_df.Source, following_dict)
+    # plot_histogram("gen-data sink following num histogram", data_df.Sink, following_dict)
+    # plot_histogram("test-public source following num histogram", test_df.Source, following_dict)
+    # plot_histogram("test-public sink following num histogram", test_df.Sink, following_dict)
+    # print()
+    # plot_histogram("gen-data source followers num histogram", data_df.Source, followers_dict)
+    # plot_histogram("gen-data sink followers num histogram", data_df.Sink, followers_dict)
+    # plot_histogram("test-public source followers num histogram", test_df.Source, followers_dict)
+    # plot_histogram("test-public sink followers num histogram", test_df.Sink, followers_dict)
     print()
     print()
 
@@ -96,14 +119,36 @@ def main ():
 
 
 
-def plot_normal_distribution(title, data_df, following_dict):
-    l = data_df.apply(lambda s: len(following_dict[s]) if s in following_dict else 1).values
+def plot_normal_distribution(title, data_series, dict):
+    l = data_series.apply(lambda s: len(dict[s]) if s in dict else 1).values
 
     mu, std = norm.fit(l)
     print(title + ": " + str(mu) + ", " + str(std))
     x = np.linspace(mu - 3 * std, mu + 3 * std, 100)
     plt.plot(x, norm.pdf(x, mu, std))
     plt.title(title)
+    plt.show()
+
+def plot_histogram(title, data_series, dict):
+    x = []
+    for key in data_series.values:
+        if key in dict and len(dict[key]) < 20000:
+            x.append(len(dict[key]))
+
+    print(title)
+    print("count: " + str(len(x)))
+    print("mean: " + str(sum(x) / len(x)))
+    print()
+
+    # x = np.random.normal(size=1000)
+    plt.title(title)
+    # bins = 10 ** (np.arange(0, 3, 0.10)) # do it yourself x axis log
+    bins = (np.arange(0, 100))
+
+    plt.hist(x, log=True, bins=bins)
+    print(plt.axis())
+    # x1, x2, y1, y2 = plt.axis()
+    # plt.axis((x1,x2,y1,200))
     plt.show()
 
 def extract_followers_following_avg(edges_df, following_dict, followers_dict):
